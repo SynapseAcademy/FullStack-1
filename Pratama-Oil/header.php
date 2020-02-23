ebe2e220aa2bc9d1f738ff04dfe52ac82ddbe0f9
SynapseAcademy <lkpsynapse@gmail.com>
2020-02-23 11:12:53 +0700

    membuat menu dinamis dan menambahkan folder view

--- Pratama-Oil/header.php
+++ Pratama-Oil/header.php
@@ -2,17 +2,17 @@
 <section class="header fixed-top">
    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container">
-         <a class="navbar-brand" href="index.html"><img src="./Assets/logo/Group 84.png" alt=""></a>
+         <a class="navbar-brand" href="index.php"><img src="./Assets/logo/Group 84.png" alt=""></a>
          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
          </button>
          <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-               <a class="nav-item nav-link active" href="index.html">TENTANG KAMI <span class="sr-only">(current)</span></a>
-               <a class="nav-item nav-link" href="single_product.html">BISNIS KAMI</a>
-               <a class="nav-item nav-link" href="#">HUBUNGAN INVESTOR</a>
-               <a class="nav-item nav-link" href="info-rekrutmen.html">KARIR</a>
-               <a class="nav-item nav-link" href="#">HUBUNGI KAMI</a>
+               <a class="nav-item nav-link active" href="?page=about">TENTANG KAMI <span class="sr-only">(current)</span></a>
+               <a class="nav-item nav-link" href="?page=business">BISNIS KAMI</a>
+               <a class="nav-item nav-link" href="?page=relationship">HUBUNGAN INVESTOR</a>
+               <a class="nav-item nav-link" href="?page=carier">KARIR</a>
+               <a class="nav-item nav-link" href="?page=contact">HUBUNGI KAMI</a>
             </div>
          </div>
    </nav>
